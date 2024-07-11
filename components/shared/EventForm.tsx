45b1cbff4ac7ffd6ccdb8b5bea0abb822c955377
Aman Aggarwal <aaggarwal2_be21@thapar.edu>
2024-07-11 15:15:57 +0530

    added image uploader functionality

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import React from 'react'
+import { useState } from 'react'
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
@@ -19,7 +20,8 @@ import { eventformSchema } from '@/lib/validator'
 import * as z from 'zod'
 import { eventDefaultValues } from '@/constants'
 import Dropdown from './Dropdown'
-
+import { Textarea } from "@/components/ui/textarea"
+import { FileUploader } from './FileUploader'
 
 
 type EventFormProps = {
@@ -29,6 +31,7 @@ type EventFormProps = {
 
 const EventForm = ({ userId, type }: EventFormProps) => {
 
+    const [files, setFiles] = useState<File[]>([])
     const initialValues = eventDefaultValues;
 
     // 1. Define your form.
@@ -77,6 +80,51 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                         )}
                     />
 
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+
+                <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                
+                                <FormControl className='h-72'>
+                                    <Textarea placeholder="Description" {...field} className='textarea rounded-2xl'/>
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+<FormField
+                        control={form.control}
+                        name="imageUrl"
+                        render={({ field }) => (
+                            <FormItem className='w-full'>
+                                
+                                <FormControl className='h-72'>
+                                    
+                                    <FileUploader
+                                    onFieldChange = {field.onChange}
+                                    imageUrl = {field.value}
+                                    setFiles = {setFiles}
+                                    />
+
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+
+                    
+
+                </div>
+
+                <div className="flex flex-col gap-5 md:flex-row">
+
+                    
                 </div>
 
 
